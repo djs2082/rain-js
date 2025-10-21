1fb189fc0d9b40b4a3da87aab9d2061e373493b5
Dilip Joshi <dilipjoshis98@gmail.com>
2025-10-21 18:12:51 +0530

    Fixed export of priority badge

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ export * from "./components/ToastContainer";
 export * from "./components/Card";
 export * from "./components/Loader";
 export * from "./http/httpClient";
-export { default as PriorityBadge } from "./components/PriorityBadge";
+export * from "./components/PriorityBadge";
 
 // Styles (optional if you want to auto-include base theme)
 import "./styles/variables.css";
